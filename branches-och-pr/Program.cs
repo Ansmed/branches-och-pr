aaae06f5367a7e1dc29318a1794abe39a7f91eda
Ansmed <anton.smedman@chasacademy.se>
2025-09-10 11:06:37 +0200

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- branches-och-pr/Program.cs
+++ branches-och-pr/Program.cs
@@ -3,8 +3,25 @@
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            
+            while (input != 0)
+            {
+                Console.WriteLine("Skriv in ett nummer för att se om det är jämnt (0 för att avsluta): ");
+                int input = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public bool isEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
