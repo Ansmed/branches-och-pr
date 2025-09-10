c552dc3da6d40716122b72449bb20974d35e8e53
Ansmed <anton.smedman@chasacademy.se>
2025-09-10 10:44:06 +0200

    Add project files.

---
+++ branches-och-pr/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace branches_och_pr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
